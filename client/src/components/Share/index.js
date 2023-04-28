45d11445b7d718cfbcc3bace0ce64df97addebaa
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-04-27 19:37:50 -0700

    added logic to share, friendsPost, friendsFeed to create a post by user

--- client/src/components/Share/index.js
+++ client/src/components/Share/index.js
@@ -1,40 +1,110 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import './index.scss';
-import React from 'react';
+
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhotoFilm, faTag, faMapMarkerAlt, faSmile } from '@fortawesome/free-solid-svg-icons';
 import person from '../../assets/images/Joshua.jpg'
-export default function Share() {
-    return (
-        <div className='shareContainer d-flex rounded-2  col-md-4 mb-3 mt-3 ms-3 p-3'
-            style={{ border: '1px solid #1a1a1a' }}>
-            <div class='shareContent container flex-wrap p-2'>
-                <div className="shareTop d-flex align-items-center">
-                    <img className='profilePic' src={person} />
-                    <input placeholder='Share something amazing...' className='shareInput border-0 w-75' />
-                </div>
-                <hr className='shareHr mt-4' />
-                <div className="shareBottom d-flex align-items-center justify-content-between">
-                    <div className="shareOptions ms-1 d-flex">
-                        <div className="shareOption d-flex align-items-center">
-                            <FontAwesomeIcon className='shareIcon' icon={faPhotoFilm} color="#c94247" />
-                            <span className='shareText'> Media</span>
-                        </div>
-                        <div className="shareOption d-flex align-items-center">
-                            <FontAwesomeIcon className='shareIcon' icon={faTag} color="#c94247" />
-                            <span className='shareText'> Tag</span>
-                        </div>
-                        <div className="shareOption d-flex align-items-center">
-                            <FontAwesomeIcon className='shareIcon' icon={faMapMarkerAlt} color="#c94247" />
-                            <span className='shareText'> Location</span>
-                        </div>
-                        <div className="shareOption d-flex align-items-center">
-                            <FontAwesomeIcon className='shareIcon' icon={faSmile} color="#c94247" />
-                            <span className='shareText'> Feeling</span>
-                        </div>
-                    </div>
-                    <button className='shareButton'>Share</button>
-                </div>
-            </div>
+
+const Share = () => {
+  const [thoughtText, setThoughtText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      try {
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [addThought, ...thoughts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // // update me object's cache
+      // const { me } = cache.readQuery({ query: QUERY_ME });
+      // cache.writeQuery({
+      //   query: QUERY_ME,
+      //   data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+      // });
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setThoughtText('');
+      setCharacterCount(0);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'thoughtText' && value.length <= 280) {
+      setThoughtText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div className='container'>
+
+
+<form className="shareContainer d-flex rounded-2 " style={{ border: '1px solid #1a1a1a' }} onSubmit={handleFormSubmit}>
+  <div className='shareContent container flex-wrap p-2'>
+    <div className="shareTop d-flex align-items-center">
+      <img className='profilePic' src={person} />
+      <input placeholder='Share something amazing...' className='shareInput border-0 w-75' name="thoughtText" value={thoughtText} style={{ lineHeight: '1.5', resize: 'vertical' }} onChange={handleChange} />
+    </div>
+    <hr className='shareHr mt-4' />
+    <div className="shareBottom d-flex align-items-center justify-content-between">
+      <div className="shareOptions ms-1 d-flex">
+        <div className="shareOption d-flex align-items-center">
+          <FontAwesomeIcon className='shareIcon' icon={faPhotoFilm} color="#c94247" />
+          <span className='shareText'> Media</span>
         </div>
-    )
-}
+        <div className="shareOption d-flex align-items-center">
+          <FontAwesomeIcon className='shareIcon' icon={faTag} color="#c94247" />
+          <span className='shareText'> Tag</span>
+        </div>
+        <div className="shareOption d-flex align-items-center">
+          <FontAwesomeIcon className='shareIcon' icon={faMapMarkerAlt} color="#c94247" />
+          <span className='shareText'> Location</span>
+        </div>
+        <div className="shareOption d-flex align-items-center">
+          <FontAwesomeIcon className='shareIcon' icon={faSmile} color="#c94247" />
+          <span className='shareText'> Feeling</span>
+        </div>
+      </div>
+      <button className='shareButton btn btn-primary btn-block py-3' type="submit">Share</button>
+    </div>
+  </div>
+  
+</form>
+
+ 
+    </div>
+  );
+};
+
+export default Share;
