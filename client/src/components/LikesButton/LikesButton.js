dfadca332ee656c2c8de6cb871ab1529d8789bf0
Brandy Posthuma <blposthuma@yahoo.com>
2023-04-30 19:23:39 -0700

    added functionality to like/dislike buttons and added some styling

--- client/src/components/LikesButton/LikesButton.js
+++ client/src/components/LikesButton/LikesButton.js
@@ -28,30 +28,3 @@ export default function App() {
     );
 }
 
-
-
-
-
-// import React from 'react';
-// class Likes extends React.Component {
-
-//     state = {
-//         likes: 0
-
-//     }
-
-//     handClick = () => {
-//         this.setState({
-//             likes: this.state.likes + 1
-//         })
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={this.handleClick}> {this.state.likes} </button>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Likes
